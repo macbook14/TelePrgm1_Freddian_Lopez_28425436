2a8cbf502b1859b94c23d926543b80366461568a
Freddian López <93230847+macbook14@users.noreply.github.com>
2022-03-14 21:33:51 -0400

    Add files via upload

---
+++ Evaluacion1.py
@@ -0,0 +1,5 @@
+#Ejercicio1
+#Cree una variable que almacene su nombre_completo.
+#Imprima en pantalla el mensaje: 'Hola Prog1, soy nombre_completo.
+NCompleto = "Freddian Elias Lopez Gomez"
+print ("Hola Prog1 soy", NCompleto)
